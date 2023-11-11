1286b0383a9191f7fca2f428f8b449ec81b98be5
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-11 01:21:50 +0100

    Camera boundaries for movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 public class PlayerController : MonoBehaviour
 {
     private Camera mainCam;
+    private Vector3 offset;
+    private float maxLeft;
+    private float maxRight;
+    private float maxDown;
+    private float maxUp;
+
     void Start()
     {
         mainCam = Camera.main;
+        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f, 0)).x;
+        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f, 0)).x;
+        maxDown = mainCam.ViewportToWorldPoint(new Vector2(0, 0.05f)).y;
+        maxUp = mainCam.ViewportToWorldPoint(new Vector2(0, 0.9f)).y;
     }
 
     void Update()
@@ -17,10 +28,19 @@ public class PlayerController : MonoBehaviour
         if (Touch.fingers[0].isActive)
         {
             Touch myTouch = Touch.activeTouches[0];
-            Vector3 touchPos = myTouch.screenPosition;
-            touchPos = mainCam.ScreenToWorldPoint(touchPos);
+            Vector3 touchPos = mainCam.ScreenToWorldPoint(myTouch.screenPosition);
 
-            transform.position = new Vector3(touchPos.x, touchPos.y, 0);
+            if (Touch.activeTouches[0].phase == TouchPhase.Began)
+            {
+                offset = touchPos - transform.position;
+            }
+
+            if (Touch.activeTouches[0].phase == TouchPhase.Moved || Touch.activeTouches[0].phase == TouchPhase.Stationary)
+            {
+                transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+            }
+
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight), Mathf.Clamp(transform.position.y, maxDown, maxUp), 0);
         }
     }
 
