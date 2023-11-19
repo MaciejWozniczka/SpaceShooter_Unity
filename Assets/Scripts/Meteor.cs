a9ea3bd8feb80c3c2893ac17e2937ad9bb5339a9
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-19 20:01:24 +0100

    Added meteors health points

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -27,7 +27,7 @@ public class Meteor : Enemy
 
     public override void DeathSequence()
     {
-        
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D otherColl) 
