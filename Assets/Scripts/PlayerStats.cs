f0b8e90f3732effeee4171af74ee77a7580dd977
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-19 20:54:36 +0100

    Updating health bar while healing

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -24,6 +24,7 @@ public class PlayerStats : MonoBehaviour
             if (Time.time > timer)
             {
                 currentHealth++;
+                healthFill.fillAmount = currentHealth / maxHealth;
                 timer += healingTimeInterval;
             }
         }
