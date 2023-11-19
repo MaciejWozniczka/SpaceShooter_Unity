4164168f1f1868f631e171dbb719b4726a62327d
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-19 19:45:12 +0100

    Meteor rotation added

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -6,16 +6,18 @@ public class Meteor : Enemy
 {
     [SerializeField] private float minSpeed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float rotateSpeed;
     private float speed;
     void Start()
     {
         speed = Random.Range(minSpeed, maxSpeed);
         rb.velocity = Vector2.down * speed;
+        rotateSpeed *= Random.Range(0.5f, 1.5f);
     }
 
     void Update()
     {
-        
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
     }
 
     public override void HurtSequence()
