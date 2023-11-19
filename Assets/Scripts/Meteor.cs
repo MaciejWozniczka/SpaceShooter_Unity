f44d8cd54ddc0778c5be2adf497d005cb58a5f5c
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-19 18:15:32 +0100

    Destroying player when collided with meteor

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Meteor : Enemy
 {
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+    private float speed;
     void Start()
     {
-        
+        speed = Random.Range(minSpeed, maxSpeed);
+        rb.velocity = Vector2.down * speed;
     }
 
     void Update()
@@ -22,5 +26,13 @@ public class Meteor : Enemy
     public override void DeathSequence()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherColl) 
+    {
+        if (otherColl.CompareTag("Player"))
+        {
+            Destroy(otherColl.gameObject);
+        }
     }
 }
